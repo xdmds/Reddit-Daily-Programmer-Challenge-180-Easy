640ec49858492de943043ed8504d3529a504ea3a
xdmds <xdmds200092@gmail.com>
2014-09-16 09:35:12 -0400

    Initial Commit

---
+++ Look_and_Say/reddit_challenge_180_easy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reddit_challenge_180_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
