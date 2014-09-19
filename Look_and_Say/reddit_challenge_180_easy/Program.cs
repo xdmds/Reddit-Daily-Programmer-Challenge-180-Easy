a279f9ffa9f049e6a32f277b5f29600f32274b8c
xdmds <xdmds200092@gmail.com>
2014-09-19 10:40:27 -0400

    look and say numbers are generated. Just have to add the ability to enter the seed and number of terms as a command line argument

--- Look_and_Say/reddit_challenge_180_easy/Program.cs
+++ Look_and_Say/reddit_challenge_180_easy/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Author: Derek Leung
+ * Description: http://www.reddit.com/r/dailyprogrammer/comments/2ggy30/9152014_challenge180_easy_looknsay/
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +15,10 @@ namespace reddit_challenge_180_easy
     {
         static void Main(string[] args)
         {
+
+            Generator g = new Generator(6, 1);
+            g.generate();
+            Console.Read();
         }
     }
 }
